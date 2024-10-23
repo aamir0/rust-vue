c841c398d5c1513f295cd51d5b440680d5988f56
Aamir Nayeem <anayeem@timelessmedical.com>
2024-10-22 22:25:09 -0400

    Test change to trigger Vue build

--- src/ui/App.vue
+++ src/ui/App.vue
@@ -14,7 +14,7 @@ import HelloWorld from './components/HelloWorld.vue'
     />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="You did it! 🥳" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
