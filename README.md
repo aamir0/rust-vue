914ab92f4bd5a2c619b004b3dfb4c20735256385
Aamir Nayeem <anayeem@timelessmedical.com>
2024-11-21 16:06:53 -0500

    Run wasm with Vite

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # rust-vue
 Set up monorepo with GitHub Actions using Rust and Vue
+
+## Building
+
+### Rust
+```
+wasm-pack build --target bundler
+```
+
+## Running
+```
+pnpm dev
+```
--- src/ui/main.ts
+++ src/ui/main.ts
@@ -6,6 +6,10 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import * as rustWasm from 'rust-vue';
+
+rustWasm.greet('WebAssembly with npm');
+
 const app = createApp(App)
 
 app.use(createPinia())
