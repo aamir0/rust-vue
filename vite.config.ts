1c89d4af2e558dbb0a1ba789c8a682d993e59eb1
Aamir Nayeem <anayeem@timelessmedical.com>
2024-11-21 21:46:26 -0500

    Add top-level-await

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,15 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import wasm from 'vite-plugin-wasm';
+import topLevelAwait from 'vite-plugin-top-level-await';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
-    wasm()
+    wasm(),
+    topLevelAwait()
   ],
   resolve: {
     alias: {
